88f6fa3ea09228aded9cf3c9f4fbd32a4710ffa4
Daniel Githinji <dgithinji331@gmail.com>
2026-07-10 00:42:34 +0300

    best

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,10 +5,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../build',
-    emptyOutDir: true,
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
